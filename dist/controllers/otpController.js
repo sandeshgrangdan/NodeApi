224c892e89b24fbd5d81f0fb7340b4b506c656cc
SoftNEP <softnep@SoftNEP-Macbook-Pro.local>
2021-10-03 00:04:08 +0545

    Api Logic Change

--- dist/controllers/otpController.js
+++ dist/controllers/otpController.js
@@ -5,7 +5,8 @@ const TODOS = [];
 const verifyOtp = (req, res, next) => {
     var _a;
     const otp = (_a = req.body.otp) !== null && _a !== void 0 ? _a : "";
-    if (otp.length === 6)
+    const arrayOtp = Array.from(otp);
+    if (otp.length === 6 && arrayOtp[arrayOtp.length - 1] !== '7')
         res.status(200).json({ status: "success", message: "Your Otp code has been verified" });
     else
         res.status(500).json({ status: "error", message: "Otp Verification Failed !!" });
--- src/controllers/otpController.ts
+++ src/controllers/otpController.ts
@@ -6,7 +6,9 @@ const TODOS: Otp[] = [];
 
 export const verifyOtp: RequestHandler = (req, res, next) => {
   const otp = (req.body as { otp: string }).otp ?? "";
-  if (otp.length === 6)
+
+  const arrayOtp = Array.from(otp)
+  if (otp.length === 6 && arrayOtp[arrayOtp.length - 1] !== '7')
     res.status(200).json({ status: "success", message: "Your Otp code has been verified" });
   else
     res.status(500).json({ status: "error", message: "Otp Verification Failed !!" });
